12282fb174ff77fa7ca949b31c0418cbf4e186c1
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-08-03 19:37:38 -0300

    addition and divition

---
+++ addition.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?= $_GET["first"]." + ". $_GET["second"]." = ". $_GET["first"]/$_GET["second"];?>
+<br>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
---
+++ divition.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?= $_GET["numerator"]." / ". $_GET["denominator"]." = ". $_GET["numerator"]/$_GET["denominator"];?>
+<br>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
